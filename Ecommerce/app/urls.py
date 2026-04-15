2385c3fc24b8962c3e4fe5108b4b30157f8541aa
Deep <deepkrushangi@gmail.com>
2026-04-15 11:43:39 +0530

    15th april

--- Ecommerce/app/urls.py
+++ Ecommerce/app/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('bestseller', views.bestseller),
+    path('cart', views.cart),
+    path('cheackout', views.cheackout),
+    path('contact', views.contact),
+    path('shop', views.shop),
+    path('single', views.single),
+
 ]
--- Ecommerce/app/views.py
+++ Ecommerce/app/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render,redirect
 
 def index(request):
     return render(request, 'index.html')
+
+def bestseller(request):
+    return render(request, 'bestseller.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def cheackout(request):
+    return render(request, 'cheackout.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def shop(request):
+    return render(request, 'shop.html')
+
+def single(request):
+    return render(request, 'single.html')
